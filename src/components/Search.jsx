4281e1997cd2b13ec78d176e07c68a7d343da89e
samsul maarif <samsulmaarif1076@gmail.com>
2025-02-03 14:40:58 +0700

    fix: bug on Search Component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Search = ({searchTerm, setSearchTerm}) => {
   return (
-    <div className='search'>{searchTerm}
+    <div className='search'>
         <div className=''>
             <img src='./search.svg' alt='search icon'/>
             <input type='text' placeholder='Search for a movie...' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
